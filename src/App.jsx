88dcf7591d5fc45d0f898bb1789d272d713c9848
makekop <72009584+makekop@users.noreply.github.com>
2026-03-11 21:22:39 +0200

    First working version. Includes - Pagination - WIP layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { CardImage } from '@/components/ui/CardImage';
+import { useState, useEffect } from 'react';
+import { PaginationSimple } from './components/ui/PaginationSimple';
+import Header from './components/ui/Header';
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const rowsPerPage = 100;
+  const [photos, setPhotos] = useState([]);
+  const [startIndex, setStartIndex] = useState(0);
+  const [endIndex, setEndIndex] = useState(20);
+
+  async function fetchPlaceholders() {
+    try {
+      const res = await fetch(
+        `https://jsonplaceholder.typicode.com/photos?_limit=${rowsPerPage}`,
+      );
+      if (!res.ok) throw new Error(`HTTP ${res.status}`);
+      const data = await res.json();
+      setPhotos(data);
+    } catch (err) {
+      console.error('Error message: ', err);
+    }
+  }
+
+  useEffect(() => {
+    fetchPlaceholders();
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div>
+      <Header />
+      <div className="grid gap-y-8 sm:grid-cols-5 lg:grid-cols-10">
+        <CardImage
+          photos={photos}
+          startIndex={startIndex}
+          endIndex={endIndex}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className="flex items-center justify-center mt-8">
+        <PaginationSimple
+          startIndex={startIndex}
+          endIndex={endIndex}
+          rowsPerPage={20}
+          setStartIndex={setStartIndex}
+          setEndIndex={setEndIndex}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
-
-export default App
---
+++ src/components/ui/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="flex items-center m-8 text-4xl font-bold">
+      The Photo Browser
+    </div>
+  );
+}
