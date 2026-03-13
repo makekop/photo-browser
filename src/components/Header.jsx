e8f478cdd5721a8a04578f851d89967953c088fa
makekop <72009584+makekop@users.noreply.github.com>
2026-03-13 16:13:08 +0200

    feat: make header fixed

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <div className="lg:fixed top-0 right-0 left-0 md:flex items-center m-8 text-4xl font-bold">
+      The Photo Browser
+    </div>
+  );
+}
