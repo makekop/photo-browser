79568daf300230639c33ff4a672c83354575bb7a
makekop <72009584+makekop@users.noreply.github.com>
2026-04-13 13:39:35 +0300

    Refactor: Add loading and error messages to client

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,21 @@
 import PhotoCard from './components/PhotoCard';
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import { SelectPage } from './components/SelectPage';
 import Header from './components/Header';
-
+import useFetch from './hooks/fetchHook';
 export const IMAGES_PER_PAGE = 20;
 export const MAX_PAGES = 5000 / IMAGES_PER_PAGE;
 
 export default function App() {
-  const [photos, setPhotos] = useState([]);
   const [page, setPage] = useState(1);
-
-  async function fetchPhotos() {
-    try {
-      const res = await fetch(
-        `https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${IMAGES_PER_PAGE}`,
-      );
-      const data = await res.json();
-      setPhotos(data);
-    } catch (err) {
-      console.error('Error message: ', err);
-    }
-  }
-
-  useEffect(() => {
-    fetchPhotos();
-  }, [page]);
+  const url = `https://jsonplaceholder.typicode.com/photos?_page=${page}&_limit=${IMAGES_PER_PAGE}`;
+  const { data: photos, loading, error } = useFetch(url);
 
   return (
     <div>
       <Header />
+      {loading && <p>Loading...</p>}
+      {error && <p>Big Error!!</p>}
       <div className="grid gap-y-8 sm:grid-cols-5 lg:grid-cols-10 pt-20 pb-10">
         {photos.map((photo) => {
           return <PhotoCard photo={photo} key={photo.id} />;
